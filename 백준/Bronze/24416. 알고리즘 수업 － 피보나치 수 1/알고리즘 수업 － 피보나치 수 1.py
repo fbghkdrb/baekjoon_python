52d59decf3fb8ca93742fb3bd3585d894c567bf6
fbghkdrb <115923132+fbghkdrb@users.noreply.github.com>
2024-07-27 19:13:29 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -1,14 +1,10 @@
-def fib(n) : 
-    global count_fib
-    if n == 1 or n == 2 :
-        count_fib += 1
-        return 1
-    else :
-        return (fib(n - 1) + fib(n - 2))
+def fibonacci(n) : 
+    f = [0] * (n+1)
+    f[1] = f[2] = 1
+    for i in range(3, n+1) :
+        f[i] = f[i-1] + f[i-2]
+    return f[n]
+
 
 n = int(input())
-f = [0] * (n+1)
-count_fib = 0
-
-fib(n)
-print(count_fib, n-2)
+print(fibonacci(n), n-2)
